bee2e24d9821ca4baa150c97e5187021ce30c55a
GreatGodOfFire <greatgodoffire@protonmail.com>
2021-04-30 22:38:36 +0200

    add packet broadcast functions

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,6 +1,9 @@
 pub mod error;
 pub mod player;
 
+use uuid::Uuid;
+
+use crate::net::error::NetError;
 use self::player::Player;
 
 pub struct Server {
@@ -15,5 +18,28 @@ impl Server {
 
     pub fn add_player(&mut self, player: Player) {
         self.players.push(player);
+    }
+
+    pub async fn broadcast_packet<P>(&mut self, id: i32, packet: P) -> Result<(), NetError> where P: serde::Serialize {
+        let packet = &packet;
+
+        for player in self.players.as_mut_slice() {
+            player.client_mut().send_packet(id, packet).await?;
+        }
+
+        Ok(())
+    }
+
+
+    pub async fn broadcast_packet_exclude<P>(&mut self, id: i32, packet: P, exclude: Vec<&Uuid>) -> Result<(), NetError> where P: serde::Serialize {
+        let packet = &packet;
+
+        for player in self.players.as_mut_slice() {
+            if !exclude.contains(&player.uuid()) {
+                player.client_mut().send_packet(id, packet).await?;
+            }
+        }
+
+        Ok(())
     }
 }
