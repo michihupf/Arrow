107af9620012149f613ea31a6d3e58fa2e1cd5b3
michihupf <lpmichi38@gmail.com>
2021-05-01 16:11:56 +0200

    commit to rebase

--- src/serde/packets/play/clientbound.rs
+++ src/serde/packets/play/clientbound.rs
@@ -1,1 +1,17 @@
+use uuid::Uuid;
+use crate::serde::types::Varint;
 
+pub struct SpawnEntity {
+    pub entity_id: Varint,
+    pub object_uuid: Uuid,
+    pub entity_type: Varint,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub pitch: f32,
+    pub yaw: f32,
+    pub data: i32,
+    pub vel_x: i16,
+    pub vel_y: i16,
+    pub vel_z: i16,
+}
--- src/serde/packets/play/mod.rs
+++ src/serde/packets/play/mod.rs
@@ -1,2 +1,29 @@
+use crate::serde::packets::play::clientbound::SpawnEntity;
+use crate::serde::types::Varint;
+use crate::server::Server;
+
+use uuid::Uuid;
+use serde::Serializer;
+
 pub mod clientbound;
 pub mod serverbound;
+
+pub async fn spawn_entity(server: Server, entity_id: Varint, object_uuid: Uuid, entity_type: Varint, position: (f32, f32, f32), pitch: f32, yaw: f32, data: i32, velocity: (i16, i16, i16)) {
+    let spawn_entity = SpawnEntity {
+        entity_id: entity_id,
+        object_uuid: object_uuid,
+        entity_type: entity_type,
+        x: position.0,
+        y: position.1,
+        z: position.2,
+        pitch: pitch,
+        yaw: yaw,
+        data: data,
+        vel_x: velocity.0,
+        vel_y: velocity.1,
+        vel_z: velocity.2,
+    };
+
+    // send the packet to every player
+    server.lock().await.broadcast_packet(0x00, spawn_entity);
+}
