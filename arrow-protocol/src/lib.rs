1016123a0cee6544b4c0d0df5eecea6eaf5d3bfe
GreatGodOfFire <greatgodoffire@protonmail.com>
2021-05-20 22:02:15 +0200

    edit example

--- arrow-protocol/src/lib.rs
+++ arrow-protocol/src/lib.rs
@@ -11,9 +11,11 @@
 //! // 754 is the protocol version of the 1.16.5 version.
 //! let bytes = packet.clone().into_packet(754).data_bytes().unwrap();
 //!
-//! # assert_eq!(bytes, vec![3, 70, 111, 111]);
+//! assert_eq!(bytes, vec![3, 70, 111, 111]);
+//!
 //! let new_packet = PacketKind::from_bytes(State::Login, true, 754, 0x0, bytes).unwrap();
-//! # assert_eq!(packet, new_packet);
+//!
+//! assert_eq!(packet, new_packet);
 //! ```
 
 #[deny(missing_docs)]
